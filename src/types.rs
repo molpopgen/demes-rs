84d8cf90a8aaf29702968a0ee3023e1998307286
Kevin R. Thornton <krthornt@uci.edu>
2022-04-25 16:49:32 -0700

    Refactor new types: (#2)
    * split up start_time and end_time
    * improve error messages
    * add more tests

--- src/types.rs
+++ src/types.rs
@@ -2,57 +2,68 @@ use serde::{Deserialize, Serialize};
 
 use std::convert::TryFrom;
 
+use crate::DemesError;
+
 #[derive(Clone, Copy, Debug, Serialize)]
 #[repr(transparent)]
-/// Representation of "time" values.
-/// The underlying `f64` must be non-negative, non-NaN.
-///
-/// # Examples
-///
-/// The type is constructed from an `f64`:
-///
-/// ```
-/// let t = demes::EpochTime::try_from(1.0).unwrap();
-/// ```
-///
-/// ```{should_panic}
-/// let t = demes::EpochTime::try_from(-1.0).unwrap();
-/// ```
-///
-/// ```{should_panic}
-/// let t = demes::EpochTime::try_from(f64::NAN).unwrap();
-/// ```
-pub struct EpochTime(f64);
+pub struct StartTime(f64);
+
+impl TryFrom<f64> for StartTime {
+    type Error = DemesError;
+
+    fn try_from(value: f64) -> Result<Self, Self::Error> {
+        if value.is_nan() || value <= 0.0 {
+            Err(DemesError::StartTimeError(value))
+        } else {
+            Ok(Self(value))
+        }
+    }
+}
+
+impl_deserialize_for_try_from_f64!(StartTime);
+
+#[derive(Clone, Copy, Debug, Serialize)]
+#[repr(transparent)]
+pub struct EndTime(f64);
+
+impl TryFrom<f64> for EndTime {
+    type Error = DemesError;
+
+    fn try_from(value: f64) -> Result<Self, Self::Error> {
+        if value.is_nan() || value.is_infinite() || value < 0.0 {
+            Err(DemesError::EndTimeError(value))
+        } else {
+            Ok(Self(value))
+        }
+    }
+}
+
+impl_deserialize_for_try_from_f64!(EndTime);
 
 #[derive(Clone, Copy, Debug, Serialize)]
 #[repr(transparent)]
 /// Representation of deme sizes.
 /// The underlying `f64` must be non-negative, non-NaN.
-///
-/// # Examples
-///
-/// The type is constructed from an `f64`:
-///
-/// ```
-/// let t = demes::DemeSize::try_from(1.0).unwrap();
-/// ```
-///
-/// ```{should_panic}
-/// let t = demes::DemeSize::try_from(-1.0).unwrap();
-/// ```
-///
-/// ```{should_panic}
-/// let t = demes::DemeSize::try_from(f64::NAN).unwrap();
-/// ```
 pub struct DemeSize(f64);
 
-impl_f64_newtypes!(EpochTime, EpochTimeError);
-impl_f64_newtypes!(DemeSize, DemeSizeError);
+impl TryFrom<f64> for DemeSize {
+    type Error = DemesError;
+
+    fn try_from(value: f64) -> Result<Self, Self::Error> {
+        if value.is_nan() || value.is_infinite() || value <= 0.0 {
+            Err(DemesError::DemeSizeError(value))
+        } else {
+            Ok(Self(value))
+        }
+    }
+}
+
+impl_deserialize_for_try_from_f64!(DemeSize);
 
 #[derive(Clone, Copy, Debug, Serialize, Deserialize)]
 pub struct TimeInterval {
-    start_time: EpochTime,
-    end_time: EpochTime,
+    start_time: StartTime,
+    end_time: EndTime,
 }
 
 #[derive(Clone, Copy, Debug, Serialize, Deserialize)]
@@ -62,11 +73,11 @@ pub struct Epoch {
 }
 
 impl Epoch {
-    pub fn start_time(&self) -> EpochTime {
+    pub fn start_time(&self) -> StartTime {
         self.time_interval.start_time
     }
 
-    pub fn end_time(&self) -> EpochTime {
+    pub fn end_time(&self) -> EndTime {
         self.time_interval.end_time
     }
 }
@@ -77,38 +88,52 @@ mod tests {
 
     #[test]
     #[should_panic]
-    fn test_negative_epoch_start_time() {
+    fn test_negative_start_time() {
         let yaml = "---\nstart_time: -1.0\nend_time: 1.1\n".to_string();
-        let ti: TimeInterval = serde_yaml::from_str(&yaml).unwrap();
-        assert_eq!(ti.start_time.0, 1.0);
-        assert_eq!(ti.end_time.0, 1.1);
+        let _: TimeInterval = serde_yaml::from_str(&yaml).unwrap();
     }
 
     #[test]
     #[should_panic]
-    fn test_nan_epoch_start_time() {
+    fn test_nan_start_time() {
         let yaml = "---\nstart_time: .nan\nend_time: 1.1\n".to_string();
-        let ti: TimeInterval = serde_yaml::from_str(&yaml).unwrap();
-        assert_eq!(ti.start_time.0, 1.0);
-        assert_eq!(ti.end_time.0, 1.1);
+        let _: TimeInterval = serde_yaml::from_str(&yaml).unwrap();
+    }
+
+    #[test]
+    fn test_zero_end_time() {
+        let yaml = "---\nstart_time: 1.0\nend_time: 0.\n".to_string();
+        let ts: TimeInterval = serde_yaml::from_str(&yaml).unwrap();
+        assert_eq!(ts.start_time.0, 1.0);
+        assert_eq!(ts.end_time.0, 0.0);
     }
 
     #[test]
     #[should_panic]
-    fn test_negative_epoch_end() {
+    fn test_zero_start_time() {
+        let yaml = "---\nstart_time: 0.0\nend_time: 1.1\n".to_string();
+        let _: TimeInterval = serde_yaml::from_str(&yaml).unwrap();
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_negative_end_time() {
         let yaml = "---\nstart_time: 1.0\nend_time: -1.1\n".to_string();
-        let ti: TimeInterval = serde_yaml::from_str(&yaml).unwrap();
-        assert_eq!(ti.start_time.0, 1.0);
-        assert_eq!(ti.end_time.0, 1.1);
+        let _: TimeInterval = serde_yaml::from_str(&yaml).unwrap();
     }
 
     #[test]
     #[should_panic]
-    fn test_nan_epoch_end() {
+    fn test_infinite_end_time() {
+        let yaml = "---\nstart_time: 1.0\nend_time: .Inf\n".to_string();
+        let _: TimeInterval = serde_yaml::from_str(&yaml).unwrap();
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_nan_epoch_end_time() {
         let yaml = "---\nstart_time: 1.0\nend_time: .nan\n".to_string();
-        let ti: TimeInterval = serde_yaml::from_str(&yaml).unwrap();
-        assert_eq!(ti.start_time.0, 1.0);
-        assert_eq!(ti.end_time.0, 1.1);
+        let _: TimeInterval = serde_yaml::from_str(&yaml).unwrap();
     }
 
     #[test]
@@ -125,5 +150,12 @@ mod tests {
         let epoch: Epoch = serde_yaml::from_str(&yaml).unwrap();
         assert_eq!(epoch.time_interval.start_time.0, 1.0);
         assert_eq!(epoch.time_interval.end_time.0, 1.1);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_deme_size_zero() {
+        let yaml = "---\n0.0\n".to_string();
+        let _: DemeSize = serde_yaml::from_str(&yaml).unwrap();
     }
 }

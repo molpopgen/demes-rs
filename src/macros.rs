28b8ce4c84b41a6ffca0aaa9dc01e920c17229b6
molpopgen <krthornt@uci.edu>
2022-05-04 15:47:23 -0700

    implement low-level traits for newtypes

---
+++ src/macros.rs
@@ -0,0 +1,34 @@
+#![macro_use]
+
+macro_rules! impl_newtype_traits {
+    ($type: ty) => {
+        impl From<$type> for f64 {
+            fn from(value: $type) -> f64 {
+                value.0
+            }
+        }
+
+        impl PartialOrd<$type> for $type {
+            fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+                Some(self.cmp(&other))
+            }
+        }
+
+        impl Ord for $type {
+            fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+                match self.0.partial_cmp(&other.0) {
+                    Some(ordering) => ordering,
+                    None => panic!("invalid f64 snuck in somehow"),
+                }
+            }
+        }
+
+        impl PartialEq for $type {
+            fn eq(&self, other: &Self) -> bool {
+                self.0 == other.0
+            }
+        }
+
+        impl Eq for $type {}
+    };
+}

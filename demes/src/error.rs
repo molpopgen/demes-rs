5c2525d5270e05891f37df81651004d7022b89c8
Kevin R. Thornton <krthornt@uci.edu>
2025-10-24 12:08:46 -0700

    refactor: manually implement std::error::Error (#459)
    * drops dependency on thiserror

--- demes/src/error.rs
+++ demes/src/error.rs
@@ -1,5 +1,3 @@
-use thiserror::Error;
-
 /// Error type for this crate.
 ///
 /// The enum fields correspond to
@@ -25,41 +23,84 @@ use thiserror::Error;
 /// assert!(matches!(demes::loads(yaml), Err(demes::DemesError::EpochError(_))));
 /// ```
 // cbindgen:no-export
-#[derive(Error, Debug)]
+#[derive(Debug)]
 #[non_exhaustive]
 pub enum DemesError {
     /// Errors related to demes
-    #[error("{0:?}")]
+    // #[error("{0:?}")]
     DemeError(String),
-    #[error("{0:?}")]
+    // #[error("{0:?}")]
     /// Errors related to epochs
     EpochError(String),
-    #[error("{0:?}")]
+    // #[error("{0:?}")]
     /// Top-level errors.
     GraphError(String),
-    #[error("{0:?}")]
+    // #[error("{0:?}")]
     /// Errors related to migrations
     MigrationError(String),
-    #[error("{0:?}")]
+    // #[error("{0:?}")]
     /// Errors related to pulses
     PulseError(String),
-    #[error(transparent)]
     /// Errors coming from `serde_yaml`.
-    YamlError(#[from] serde_yaml::Error),
+    YamlError(serde_yaml::Error),
     #[cfg(feature = "json")]
     #[cfg_attr(doc_cfg, doc(cfg(feature = "json")))]
-    #[error(transparent)]
     /// Errors coming from `serde_json`.
-    JsonError(#[from] serde_json::Error),
+    JsonError(serde_json::Error),
     #[cfg(feature = "toml")]
     #[cfg_attr(doc_cfg, doc(cfg(feature = "toml")))]
-    #[error(transparent)]
     /// Errors coming from `toml` during deserialization.
-    TomlDeError(#[from] toml::de::Error),
+    TomlDeError(toml::de::Error),
     /// Errors related to low-level types
-    #[error("{0:?}")]
+    // #[error("{0:?}")]
     ValueError(String),
     /// IO errors from the rust standard library
-    #[error(transparent)]
-    IOerror(#[from] std::io::Error),
+    IOerror(std::io::Error),
 }
+
+impl From<serde_yaml::Error> for DemesError {
+    fn from(value: serde_yaml::Error) -> Self {
+        Self::YamlError(value)
+    }
+}
+
+impl From<std::io::Error> for DemesError {
+    fn from(value: std::io::Error) -> Self {
+        Self::IOerror(value)
+    }
+}
+
+#[cfg(feature = "json")]
+impl From<serde_json::Error> for DemesError {
+    fn from(value: serde_json::Error) -> Self {
+        Self::JsonError(value)
+    }
+}
+
+#[cfg(feature = "toml")]
+impl From<toml::de::Error> for DemesError {
+    fn from(value: toml::de::Error) -> Self {
+        Self::TomlDeError(value)
+    }
+}
+
+impl std::fmt::Display for DemesError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            DemesError::DemeError(e) => write!(f, "deme error: {}", e),
+            DemesError::EpochError(e) => write!(f, "epoch error: {}", e),
+            DemesError::GraphError(e) => write!(f, "graph error: {}", e),
+            DemesError::MigrationError(e) => write!(f, "migration error: {}", e),
+            DemesError::PulseError(e) => write!(f, "pulse error: {}", e),
+            DemesError::YamlError(e) => write!(f, "yaml error: {}", e),
+            DemesError::ValueError(e) => write!(f, "value error: {}", e),
+            DemesError::IOerror(e) => write!(f, "io error: {}", e),
+            #[cfg(feature = "json")]
+            DemesError::JsonError(e) => write!(f, "JSON error: {e:?}"),
+            #[cfg(feature = "toml")]
+            DemesError::TomlDeError(e) => write!(f, "TOML deserialization error: {e:?}"),
+        }
+    }
+}
+
+impl std::error::Error for DemesError {}
